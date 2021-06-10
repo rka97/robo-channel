709905ce18600223ae6f1fc107d36dd46e3a9ae0
rka97 <ahmed.khaled@princeton.edu>
2021-06-11 01:18:01 +0200

    small

---
+++ robo-guix/robo-fonts.scm
@@ -0,0 +1,53 @@
+(define-module (robo-guix robo-fonts)
+  #:use-module (ice-9 regex)
+  #:use-module (guix utils)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system font)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system meson)
+  #:use-module (guix build-system trivial)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages xorg))
+
+(define-public font-iosevka-ss13
+  (package
+    (name "font-iosevka-ss13")
+    (version "7.0.4")
+    (source
+     (origin
+       (method url-fetch/zipbomb)
+       (uri (string-append "https://github.com/be5invis/Iosevka"
+                           "/releases/download/v" version
+                           "/super-ttc-iosevka-ss13-" version ".zip"))
+       (sha256
+        (base32 "1cngyrl8q88f9q9kkp8y36jm3d8swr4mh83ccpw9lm9r949b708f"))))
+    (build-system font-build-system)
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (add-before 'install 'make-files-writable
+           (lambda _
+             (for-each make-file-writable (find-files "." ".*"))
+             #t)))))
+    (home-page "https://be5invis.github.io/Iosevka/")
+    (synopsis "Coders' typeface, built from code")
+    (description
+     "Iosevka is a slender monospace sans-serif or slab-serif typeface inspired
+by Pragmata Pro, M+, and PF DIN Mono, designed to be the ideal font for
+programming.  Iosevka is completely generated from its source code.")
+    (license (list license:silofl1.1 ; build artifacts (i.e. the fonts)
+                   license:bsd-3))
+    ))
